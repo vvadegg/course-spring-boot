fcb3e97d5b60211077b948d0e11509b69a1b176a
vvadegg <vadikbel@list.ru>
2020-06-03 14:21:38 +0300

    init commit

---
+++ src/main/java/com/example/sweater/domain/Message.java
@@ -0,0 +1,34 @@
+package com.example.sweater.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+
+    private String text;
+
+    private String tag;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    public Message() {}
+    public Message(String text, String tag, User user) {
+        this.text = text;
+        this.tag = tag;
+        this.author = user;
+    }
+
+    public String getAuthorName() {
+        return author != null ? author.getUsername() : "<none>";
+    }
+
+}
